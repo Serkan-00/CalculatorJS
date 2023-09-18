b500fb176011b4c2e4118a6846b0f13968bc7436
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-18 14:40:33 +0100

    calculator done

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const screenDisplay = document.querySelector('.screen')
 const buttons = document.querySelectorAll('button')
-const screenDisplay = document.querySelectorAll('.screen')
+
 
 let calculation = []
+let accumulativeCalculation
 
 function calculate(button) { 
     const value = button.textContent
 
-    calculation.push(value)
+    if (value == "CLEAR") { 
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if (value === "=") { 
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
 
-    console.log(calculation)
+    
 }
 
 buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
